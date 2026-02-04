c5aa8bb53ca97976d2060b96c60352d80719bb6d
Intervention128 <intervention128@gmail.com>
2026-02-04 07:34:37 +0100

    fix: nocheckpercent now actually works

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,21 @@
-function disableButton() {
-  const buttonClassBecauseItDoesntHaveAnId = "._button_cmw0a_1"
-  const button = document.querySelector(buttonClassBecauseItDoesntHaveAnId)
+const buttonClassBecauseItDoesntHaveAnId = "._button_cmw0a_1"
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function disableButton() {
+  let button = document.querySelector(buttonClassBecauseItDoesntHaveAnId)
+  do {
+    await sleep(20)
+    button = document.querySelector(buttonClassBecauseItDoesntHaveAnId)
+  } while (!button)
+  console.log(button)
   button.style.pointerEvents  = "none"
   button.style.display = "none"
 }
 
 void function run() {
   disableButton()
+  console.log("NoCheckPercent enabled.")
 }()
